7bf1def514cfbe443a433dc21c266cb9d58116c5
franmassimino <franmassi1904@gmail.com>
2021-07-29 19:12:06 -0300

    heroku deploy fixes ?2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3030
-const host = '0.0.0.0';
 
 //static files
 const publicPath = path.resolve(__dirname, '../public')
@@ -15,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 //server
 app.set("port", process.env.PORT || port)
-app.listen(port, host,() => console.log("Server live on http://localhost:"+ port))
+app.listen(app.get("port"),() => console.log("Server live on http://localhost:"+app.get("port")))
 
 
 //routes
